8f074433cd73deb6ebfd489d1f13c5ca969ca038
Mohamed Omar Moussa <mohamedomarmoussa02@gmail.com>
2023-08-22 12:39:44 +0200

    resolve

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,45 +1,50 @@
 class LocationsController < ApplicationController
 
-  before_action :set_location, only: [:show, :edit, :update, :destroy]
+    before_action :set_location, only: [:show, :edit, :update, :destroy]
 
-def index
-  @locations = Location.all
-end
+  def index
+    @locations = Location.all
+  end
 
-def show
-end
+  def show
+  end
 
-def new def
-  @location = Location.new
-end
+  def new
+    @location = Location.new
+  end
 
-def edit
-end
+  def edit
+  end
 
-def create
-@location = Location.new (location_params)
-if @location.save
-  redirect_to @location notice "location created"
-else render :new, status: :unprocessable_entity
-end
+  def create
+    @location = Location.new (location_params)
+    if @location.save
+      redirect_to :poissons_path, notice: "location created"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
 
-def update
-if @location.update
-  redirect_to @location notice "location updated"
-else render :new, status: :unprocessable_entity
-end
+  def update
+    if @location.update
+      redirect_to :poissons_path, notice: "location updated"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
 
-def destroy
-@location.destroy
-redirect_to locations_url, notice : location "cancelled!"
+  def destroy
+    @location.destroy
+    redirect_to :locations_path, notice: "cancelled!"
+  end
 
-private
+  private
 
-def set_location
-@location = Location.find(params:[id])
-end
+  def set_location
+    @location = Location.find(params:[id])
+  end
 
-def location_params
-params.require(:location).permit(:begin_date, :end_date, :poisson_id, :user_id)
-
+  def location_params
+    params.require(:location).permit(:begin_date, :end_date, :poisson_id, :user_id)
+  end
 end
