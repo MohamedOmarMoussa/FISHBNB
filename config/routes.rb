95c1ce6d0617e64429694480343469877a0639ee
lenny djafari <djafari.lenny@gmail.com>
2023-08-22 18:03:22 +0200

    modification-footer-poisson-routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "pages#home"
-  get "poissons", to: "poissons#index"
+  root to: "poissons#index"
+  # get "poissons", to: "poissons#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
