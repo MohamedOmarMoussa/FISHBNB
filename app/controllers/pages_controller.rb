6c70e2833a8e41edc10efa982781de8804da2774
Mohamed Omar Moussa <mohamedomarmoussa02@gmail.com>
2023-08-22 10:50:41 +0200

    controllers

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,7 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
   def home
+    @poissons = Poisson.all
+    @locations = Location.where(user: current_user)
   end
 end
