8e0acac89bf185b9c693896ed22860ab49bf856e
rouguietou kande <rouguirka@gmail.com>
2023-08-24 15:44:19 +0200

    addsearchmap

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :poissons, dependent: :destroy
+  has_many :locations, dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
