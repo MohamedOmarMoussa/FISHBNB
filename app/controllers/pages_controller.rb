a5c84a04e809c147e720a0af16a6652990cd662a
lenny djafari <djafari.lenny@gmail.com>
2023-08-25 12:44:02 +0200

    tkpage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
   def home
-    @poissons = Poisson.all
+    @poissons = Poisson.all.first(3)
     @locations = Location.where(user: current_user)
   end
 end
